9f8d961712d2b0817b8e3cc68d0e72618cb44f86
vish2001 <vishwanath01k@gmail.com>
2022-06-26 18:04:03 +0530

    Updates

---
+++ precache-manifest.3139475f928f80465c069275b87e5fc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d794b7381c61a4dd8d5d583b4f6d709b",
+    "url": "/react-ros-bot/index.html"
+  },
+  {
+    "revision": "f1e6ada277b440488193",
+    "url": "/react-ros-bot/static/css/main.d71cbdff.chunk.css"
+  },
+  {
+    "revision": "4bc3f8cc1f918cf47f17",
+    "url": "/react-ros-bot/static/js/2.bda03c0c.chunk.js"
+  },
+  {
+    "revision": "c5ff2703353d678c9d7c510e4fa0f59b",
+    "url": "/react-ros-bot/static/js/2.bda03c0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8f3b58a60e8e27ac233",
+    "url": "/react-ros-bot/static/js/3.d23a3a1a.chunk.js"
+  },
+  {
+    "revision": "f1e6ada277b440488193",
+    "url": "/react-ros-bot/static/js/main.fbaac209.chunk.js"
+  },
+  {
+    "revision": "6732cd80d0f1e0788a0e",
+    "url": "/react-ros-bot/static/js/runtime-main.795a8386.js"
+  }
+]);
